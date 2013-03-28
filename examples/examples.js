71d0be0a993a04416843a0f59fa5a2cd113b618d
Mikko Koski <mikkokos@gmail.com>
2013-03-29 00:42:28 +0200

    Add pow examples

---
+++ examples/examples.js
@@ -0,0 +1,45 @@
+var pat = require("../lib/pat");
+var _ = require("lodash");
+
+function pow1(x, y) {
+  if(!_.isNumber(x) || !_.isNumber(y)) {
+    throw new Error("Illegal arguments");
+  } else if(y === 0) {
+    return 1;
+  } else {
+    var n = x;
+    for(var i = 1; i < y; i++) {
+      n = n * x;
+    }
+    return n;
+  }
+}
+
+function pow2(x, y) {
+  if(!_.isNumber(x) || !_.isNumber(y)) {
+    throw new Error("Illegal arguments");
+  } else if(y === 0) {
+    return 1;
+  } else {
+    return x * pow1(x, y - 1);
+  }
+}
+
+var pow3 = pat()
+  .caseof(_.isNumber, 0, function() { return 1; })
+  .caseof(_.isNumber, _.isNumber, function(x, y) { 
+    return x * pow2(x, y - 1 ); 
+  });
+
+var pow4 = pat(function(x, y) {
+    return x * pow3(x, y - 1); 
+  }).caseof(_.isNumber, 0, function() {
+    return 1;
+  });
+
+module.exports = Object.freeze({
+  pow1: pow1,
+  pow2: pow2,
+  pow3: pow3,
+  pow4: pow4
+});
---
+++ test/examples_test.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var examples = require("../examples/examples");
+var expect = require("expect.js");
+
+describe("examples", function(){
+  it("pow", function() {
+    function test(fn) {
+      expect(fn(2, 0)).to.be(1);
+      expect(fn(2, 1)).to.be(2);
+      expect(fn(2, 2)).to.be(4);
+      expect(fn(2, 3)).to.be(8);
+      expect(fn(2, 4)).to.be(16);
+      expect(fn(2, 5)).to.be(32);
+      expect(fn(2, 6)).to.be(64);
+      expect(fn(2, 7)).to.be(128);
+      expect(fn(2, 8)).to.be(256);
+      expect(fn(2, 9)).to.be(512);
+      expect(fn(2, 10)).to.be(1024);
+    }
+
+    test(examples.pow1);
+    test(examples.pow2);
+    test(examples.pow3);
+    test(examples.pow4);
+  })
+});
