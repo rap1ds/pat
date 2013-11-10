4bf53e1984e6b426457a89fe4cae13144ad90371
Mikko Koski <mikkokos@gmail.com>
2013-11-10 22:49:55 +0200

    Bower example

--- examples/examples.js
+++ examples/examples.js
@@ -1,147 +1,152 @@
-/*jshint node:true */
-var pat = require("../lib/pat");
-var _ = require("lodash");
+(function(global) {
 
-/**
- * For-loop, no pattern mathing
- */
-function pow1(x, y) {
-  if(!_.isNumber(x) || !_.isNumber(y)) {
-    throw new Error("Illegal arguments");
-  } else if(y === 0) {
-    return 1;
-  } else {
-    var isNegative = y < 0;
-    y = y < 0 ? y * (-1) : y;
-    var n = x;
-    for(var i = 1; i < y; i++) {
-      n = n * x;
+  /**
+   * For-loop, no pattern mathing
+   */
+  function pow1(x, y) {
+    if(!_.isNumber(x) || !_.isNumber(y)) {
+      throw new Error("Illegal arguments");
+    } else if(y === 0) {
+      return 1;
+    } else {
+      var isNegative = y < 0;
+      y = y < 0 ? y * (-1) : y;
+      var n = x;
+      for(var i = 1; i < y; i++) {
+        n = n * x;
+      }
+      return isNegative ? 1 / n : n;
     }
-    return isNegative ? 1 / n : n;
   }
-}
 
-/**
- * Recursive, no pattern mathing
- */
-function pow2(x, y) {
-  if(!_.isNumber(x) || !_.isNumber(y)) {
-    throw new Error("Illegal arguments");
-  } else if(y === 0) {
-    return 1;
+  /**
+   * Recursive, no pattern mathing
+   */
+  function pow2(x, y) {
+    if(!_.isNumber(x) || !_.isNumber(y)) {
+      throw new Error("Illegal arguments");
+    } else if(y === 0) {
+      return 1;
+    } else {
+      var isNegative = y < 0;
+      y = y < 0 ? y * (-1) : y;
+      var result = x * pow2(x, y - 1);
+      return isNegative ? 1 / result : result;
+    }
+  }
+
+  function lessThan(a) {
+    return function(b) {
+      return _.isNumber(b) && b < a;
+    }
+  }
+
+  /**
+   * Pattern matching. Throws error if no pattern is matched.
+   */
+  var pow3 = pat()
+    .caseof(Number, 0, function() { return 1; })
+    .caseof(Number, lessThan(0), function(x, y) { return 1 / pow3(x, (y * (-1))) })
+    .caseof(Number, Number, function(x, y) { 
+      return x * pow3(x, y - 1 ); 
+    });
+
+  /**
+   * Different pattern mathing syntax. Doesn't throw error.
+   */
+  var pow4 = pat(function(x, y) {
+      return x * pow4(x, y - 1); 
+    })
+    .caseof(Number, 0, function() {
+      return 1;
+    })
+    .caseof(Number, lessThan(0), function(x, y) {
+      return 1 / pow4(x, (y * (-1)));
+    });
+
+  /*
+    Let's write a `sum` function, which returns the sum of the given
+    arguments. It is very liberal with the arguments, and can take the following:
+
+    - any number of ints
+    - int array
+    - any number of strings
+    - string array
+
+    If no arguments is given to `sum`, return 0
+    If empty array is given, return 0
+
+    Otherwise, throw "Illegal arguments" exception
+
+    Usage:
+
+    sum(0, 1, 2, 3, 4, 5) === 15;
+    sum("0", "1", "2", "3", "4", "5") === 15;
+    sum([0, 1, 2, 3, 4, 5]) === 15;
+    sum(["0", "1", "2", "3", "4", "5"]) === 15;
+    sum() === 0;
+    sum([]) === 0;
+
+  */
+
+  /**
+   * No pattern mathing
+   */
+  function sum1() {
+    var args = _.toArray(arguments);
+
+    if(!args.length) {
+      return 0;
+    } 
+    if(_.isArray(args[0])) {
+      args = args[0];
+    } 
+    if(args.every(_.isString)) {
+      args = args.map(function(n) { return parseInt(n, 10)});
+    }
+    if(args.every(_.isNumber)) {
+      return args.reduce(function(a, b) { return a + b }, 0);
+    } else {
+      throw "Illegal arguments";
+    }
+  }
+
+  var arrayOf = function(fn) {
+    return function(arr) {
+      return arr.every(fn);
+    }
+  }
+
+  /**
+   * With pattern mathing
+   */
+  var sum2 = pat()
+    .caseof(function() { return 0 })
+    .caseof([], function() { return 0 })
+    .caseof(pat.rest(_.isString), function(rest) {
+      return sum2(rest); })
+    .caseof(pat.rest(_.isNumber), function(rest) {
+      return sum2(rest); })
+    .caseof(arrayOf(_.isString), function(stringArray) {
+      return sum2(stringArray.map(function(n) {
+        return parseInt(n, 10); }));
+    })
+    .caseof(arrayOf(_.isNumber), function(intArray) {
+      return intArray.reduce(function(a, b) { return a + b }, 0);
+    });
+
+  var myMathLib = Object.freeze({
+    pow1: pow1,
+    pow2: pow2,
+    pow3: pow3,
+    pow4: pow4,
+    sum1: sum1,
+    sum2: sum2
+  });
+
+  if(typeof module !== "undefined") {
+    module.exports = myMathLib;
   } else {
-    var isNegative = y < 0;
-    y = y < 0 ? y * (-1) : y;
-    var result = x * pow2(x, y - 1);
-    return isNegative ? 1 / result : result;
+    global.myMathLib = myMathLib;
   }
-}
-
-function lessThan(a) {
-  return function(b) {
-    return _.isNumber(b) && b < a;
-  }
-}
-
-/**
- * Pattern matching. Throws error if no pattern is matched.
- */
-var pow3 = pat()
-  .caseof(Number, 0, function() { return 1; })
-  .caseof(Number, lessThan(0), function(x, y) { return 1 / pow3(x, (y * (-1))) })
-  .caseof(Number, Number, function(x, y) { 
-    return x * pow3(x, y - 1 ); 
-  });
-
-/**
- * Different pattern mathing syntax. Doesn't throw error.
- */
-var pow4 = pat(function(x, y) {
-    return x * pow4(x, y - 1); 
-  })
-  .caseof(Number, 0, function() {
-    return 1;
-  })
-  .caseof(Number, lessThan(0), function(x, y) {
-    return 1 / pow4(x, (y * (-1)));
-  });
-
-/*
-  Let's write a `sum` function, which returns the sum of the given
-  arguments. It is very liberal with the arguments, and can take the following:
-
-  - any number of ints
-  - int array
-  - any number of strings
-  - string array
-
-  If no arguments is given to `sum`, return 0
-  If empty array is given, return 0
-
-  Otherwise, throw "Illegal arguments" exception
-
-  Usage:
-
-  sum(0, 1, 2, 3, 4, 5) === 15;
-  sum("0", "1", "2", "3", "4", "5") === 15;
-  sum([0, 1, 2, 3, 4, 5]) === 15;
-  sum(["0", "1", "2", "3", "4", "5"]) === 15;
-  sum() === 0;
-  sum([]) === 0;
-
-*/
-
-/**
- * No pattern mathing
- */
-function sum1() {
-  var args = _.toArray(arguments);
-
-  if(!args.length) {
-    return 0;
-  } 
-  if(_.isArray(args[0])) {
-    args = args[0];
-  } 
-  if(args.every(_.isString)) {
-    args = args.map(function(n) { return parseInt(n, 10)});
-  }
-  if(args.every(_.isNumber)) {
-    return args.reduce(function(a, b) { return a + b }, 0);
-  } else {
-    throw "Illegal arguments";
-  }
-}
-
-var arrayOf = function(fn) {
-  return function(arr) {
-    return arr.every(fn);
-  }
-}
-
-/**
- * With pattern mathing
- */
-var sum2 = pat()
-  .caseof(function() { return 0 })
-  .caseof([], function() { return 0 })
-  .caseof(pat.rest(_.isString), function(rest) {
-    return sum2(rest); })
-  .caseof(pat.rest(_.isNumber), function(rest) {
-    return sum2(rest); })
-  .caseof(arrayOf(_.isString), function(stringArray) {
-    return sum2(stringArray.map(function(n) {
-      return parseInt(n, 10); }));
-  })
-  .caseof(arrayOf(_.isNumber), function(intArray) {
-    return intArray.reduce(function(a, b) { return a + b }, 0);
-  });
-
-module.exports = Object.freeze({
-  pow1: pow1,
-  pow2: pow2,
-  pow3: pow3,
-  pow4: pow4,
-  sum1: sum1,
-  sum2: sum2
-});
+})(this);
---
+++ examples/node/node.js
@@ -0,0 +1,11 @@
+/*jshint node:true */
+var pat = require("pat");
+
+var myMathLib = require("../examples");
+
+console.log("pow1(2, 2) =>", myMathLib.pow1(2, 2));
+console.log("pow2(3, 3) =>", myMathLib.pow1(3, 3));
+console.log("pow3(2, 3) =>", myMathLib.pow1(2, 3));
+console.log("pow4(4, 2) =>", myMathLib.pow1(4, 2));
+console.log("sum1('9', '2', '11') =>", myMathLib.sum1(['9', '2', '11']));
+console.log("sum2(9, 2, 11) =>", myMathLib.sum2([9, 2, 11]));
